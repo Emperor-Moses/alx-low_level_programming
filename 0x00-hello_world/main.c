10b5715fc315963836b8b8b371bb78bbf344e391
Moses Obinna <mobinna44@gmail.com>
2022-12-09 12:51:07 +0000

    Finaly done

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("int is %2d bytes \n", sizeof(short int));
+	printf("long int is %2d bytes \n", sizeof(long int));
+	printf("float is %2d bytes \n", sizeof(float));
+	printf("double is %2d bytes \n", sizeof(double));
+	printf("long double is %2d bytes \n", sizeof(long double));
 	return(0);
 }
