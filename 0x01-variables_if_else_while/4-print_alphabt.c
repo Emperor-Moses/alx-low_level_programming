2d85e92b791e43f55f6b7c498fd309f701128861
Moses Obinna <mobinna44@gmail.com>
2022-12-12 08:49:48 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+	   char ch;
+
+	      for(ch = 'a' ; ch <= 'z' ; ch++) {
+		     if (ch=='e') {
+			    ch+0;
+		     } else if (ch=='q') {
+			    ch+0;
+		     } else {
+			    putchar(ch);
+		     }
+	      } 
+			       
+	         
+	         return(0);
+}
---
+++ 4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	   char ch;
+
+	      for(ch = 'a' ; ch <= 'z' ; ch++) {
+		      if (ch=='e') {
+			      ch++;
+		      } else if (ch=='q') {
+			      ch++;
+		      } else {
+			      putchar(ch);
+	         
+	         return(0);
+}
