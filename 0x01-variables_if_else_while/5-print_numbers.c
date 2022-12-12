030070f493359788ef0d9880485fc60d72196e16
Moses Obinna <mobinna44@gmail.com>
2022-12-12 09:34:50 +0000

    tasks 4 and 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	n = 0;
+
+	while (n<=9) {
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	n=0;
+
+	while(n<=9) {
+		putchar(n + '0');
+
+		n++;
+	}
+	putchar("\n");
+
+	return (0);
+}
